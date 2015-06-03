68dd7799b7e6425ae155278ab3df86113523b84f
Carlos Gallardo <Carlos Gallardo@SNAP-UIO-CGALLARDO>
2015-06-03 11:08:12 -0500

    cambio en el modelo de tramiete Institucion

---
+++ protected/models/TramiteInstitucion.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class TramiteInstitucion extends CActiveRecord
+{
+    private $connection;
+    
+    public function __construct() {
+        //Yii::app()->db->connectionString
+        // 
+        $this->connection=new CDbConnection(Yii::app()->db->connectionString,Yii::app()->db->username,Yii::app()->db->password);
+        $this->connection->active=TRUE;
+    }
+    
+    public static function model($className=__CLASS__){
+        return parent::model($className);
+                
+    }
+    public function getTramiteInstitucion(){
+        $sql = "select b.trai_id , a.tra_nombre 
+from tramite a, tramite_institucion b, institucion c
+where a.tra_id = b.tra_id 
+and c.ins_id = b.ins_id
+and c.ins_id = 1
+order by a.tra_nombre";
+        $dataReader = $this->connection->createCommand($sql)->query();
+        // recibe los datos
+        $rows=$this->connection->createCommand($sql)->queryAll();
+        return $rows;
+    }
+            
+            
+    
+}
