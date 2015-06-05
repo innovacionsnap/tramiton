76fb0e6065a65025169fd98e06d6446ab3783c4e
Carlos Gallardo <Carlos Gallardo@SNAP-UIO-CGALLARDO>
2015-06-05 12:01:54 -0500

    tpl_navigation_admin

--- themes/tramiton/views/layouts/tpl_navigation_admin.php
+++ themes/tramiton/views/layouts/tpl_navigation_admin.php
@@ -16,13 +16,7 @@
 		<!-- begin header navigation right -->
 		
 		<ul class="nav navbar-nav navbar-right">
-			<!--
-			<li class=""><?php //echo CHtml::link('Inicio',array('/site/page', 'view'=>'home2')); ?></li>
-			<li class=""><?php //echo CHtml::link('Ranking',array('/site/page', 'view'=>'home2')); ?></li>
-			<li class=""><?php //echo CHtml::link('Mejora de Trámites',array('/site/page', 'view'=>'home2')); ?></li>
-			<li class=""><?php //echo CHtml::link('Acerca de Tramitón',array('/site/page', 'view'=>'home2')); ?></li>
-			<li class=""><?php //echo CHtml::link('Contacto',array('/site/page', 'view'=>'home2')); ?></li>
-			-->
+			
 			<li>
 				<form class="navbar-form full-width">
 					<div class="form-group">
@@ -31,47 +25,8 @@
 					</div>
 				</form>
 			</li>
-			<!--
-			<li class="dropdown">
-				<a href="javascript:;" data-toggle="dropdown" class="dropdown-toggle f-s-14">
-					<i class="fa fa-bell-o"></i>
-					<span class="label">5</span>
-				</a>
-				<ul class="dropdown-menu media-list pull-right animated fadeInDown">
-                    <li class="dropdown-header">Notifications (5)</li>
-                    <li class="media">
-                        <a href="javascript:;">
-                            <div class="media-left"><i class="fa fa-bug media-object bg-red"></i></div>
-                            <div class="media-body">
-                                <h6 class="media-heading">Server Error Reports</h6>
-                                <div class="text-muted f-s-11">3 minutes ago</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="media">
-                        <a href="javascript:;">
-                            <div class="media-left"><img src="<?php // echo Yii::app()->theme->baseUrl;?>/assets/img/user-1.jpg" class="media-object" alt="" /></div>
-                            <div class="media-body">
-                                <h6 class="media-heading">John Smith</h6>
-                                <p>Quisque pulvinar tellus sit amet sem scelerisque tincidunt.</p>
-                                <div class="text-muted f-s-11">25 minutes ago</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="media">
-                        <a href="javascript:;">
-                            <div class="media-left"><img src="<?php // echo Yii::app()->theme->baseUrl;?>/assets/img/user-2.jpg" class="media-object" alt="" /></div>
-                            <div class="media-body">
-                                <h6 class="media-heading">Olivia</h6>
-                                <p>Quisque pulvinar tellus sit amet sem scelerisque tincidunt.</p>
-                                <div class="text-muted f-s-11">35 minutes ago</div>
-                            </div>
-                        </a>
-                    </li>
-                    
-				</ul>
-			</li>
-			-->
+			
+		
 			<li class="dropdown navbar-user">
 				<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
 					<img src="<?php echo Yii::app()->theme->baseUrl;?>/assets/img/user-13.jpg" alt="" /> 
