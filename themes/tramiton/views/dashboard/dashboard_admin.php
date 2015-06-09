0fb36521e5fe2064f72d10c63f71741667ee2118
Carlos Gallardo <Carlos Gallardo@SNAP-UIO-CGALLARDO>
2015-06-09 11:35:21 -0500

    tpl_navigation_admin

---
+++ themes/tramiton/views/dashboard/dashboard_admin.php
@@ -0,0 +1,168 @@
+<!-- begin #content -->
+		<div id="content" class="content">
+			<!-- begin breadcrumb -->
+			<ol class="breadcrumb pull-right">
+				<li><a href="javascript:;">Home</a></li>
+				<li class="active">Tramites</li>
+			</ol>
+			<!-- end breadcrumb -->
+			<!-- begin page-header -->
+			<h1 class="page-header">Tramites <small> de la institucion</small></h1>
+
+			<!-- end page-header -->
+                        
+                        
+                        
+                        <!-- begin row -->
+			<div class="row">
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-green">
+						<div class="stats-icon"><i class="fa fa-desktop"></i></div>
+						<div class="stats-info">
+							<h4>TOTAL TRAMITES </h4>
+                                                        <p><?php foreach ($datosTotalTramites as $dato){echo $dato["total_tramite"];}  ?></p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">Ver Detalles <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-blue">
+						<div class="stats-icon"><i class="fa fa-chain-broken"></i></div>
+						<div class="stats-info">
+							<h4>RANKING TRAMITES</h4>
+							<p><?php //ech $datosRankingTramites ?>20.44%</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">Ver Datalles <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-purple">
+						<div class="stats-icon"><i class="fa fa-users"></i></div>
+						<div class="stats-info">
+							<h4>TRAMITES MAS VISITADOS</h4>
+							<p>1,291,922</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">Ver Datalles <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-red">
+						<div class="stats-icon"><i class="fa fa-clock-o"></i></div>
+						<div class="stats-info">
+							<h4>SOLUCIONES MAS VOTADAS</h4>
+							<p>30%</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">Ver Datalles <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+			</div>
+			<!-- end row -->
+                        
+                        
+			
+		<!-- begin row -->
+		<div class="row">
+		<!-- begin col-8 -->
+				<div class="col-md-12">
+					<div class="panel panel-inverse" data-sortable-id="index-1">
+						<div class="panel-heading">
+							<div class="panel-heading-btn">
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+							</div>
+							<h4 class="panel-title">Tramites ingresados (7 días)</h4>
+						</div>
+						<div class="panel-body">
+							<div id="interactive-chart" class="height-sm"></div>
+						</div>
+					</div>
+					
+					
+					<div class="panel panel-inverse" data-sortable-id="index-5">
+						<div class="panel-heading">
+							<div class="panel-heading-btn">
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+								<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+							</div>
+							<h4 class="panel-title">Mensajes</h4>
+						</div>
+						<div class="panel-body">
+							<div class="height-sm" data-scrollbar="true">
+								<ul class="media-list media-list-with-divider media-messaging">
+									<li class="media media-sm">
+										<a href="javascript:;" class="pull-left">
+											<img src="assets/img/user-5.jpg" alt="" class="media-object rounded-corner" />
+										</a>
+										<div class="media-body">
+											<h5 class="media-heading">John Doe</h5>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nunc non eros fermentum vestibulum ut id felis. Nunc molestie libero eget urna aliquet, vitae laoreet felis ultricies. Fusce sit amet massa malesuada, tincidunt augue vitae, gravida felis.</p>
+										</div>
+									</li>
+									<li class="media media-sm">
+										<a href="javascript:;" class="pull-left">
+											<img src="assets/img/user-6.jpg" alt="" class="media-object rounded-corner" />
+										</a>
+										<div class="media-body">
+											<h5 class="media-heading">Terry Ng</h5>
+											<p>Sed in ante vel ipsum tristique euismod posuere eget nulla. Quisque ante sem, scelerisque iaculis interdum quis, eleifend id mi. Fusce congue leo nec mauris malesuada, id scelerisque sapien ultricies.</p>
+										</div>
+									</li>
+									<li class="media media-sm">
+										<a href="javascript:;" class="pull-left">
+											<img src="assets/img/user-8.jpg" alt="" class="media-object rounded-corner" />
+										</a>
+										<div class="media-body">
+											<h5 class="media-heading">Fiona Log</h5>
+											<p>Pellentesque dictum in tortor ac blandit. Nulla rutrum eu leo vulputate ornare. Nulla a semper mi, ac lacinia sapien. Sed volutpat ornare eros, vel semper sem sagittis in. Quisque risus ipsum, iaculis quis cursus eu, tristique sed nulla.</p>
+										</div>
+									</li>
+									<li class="media media-sm">
+										<a href="javascript:;" class="pull-left">
+											<img src="assets/img/user-7.jpg" alt="" class="media-object rounded-corner" />
+										</a>
+										<div class="media-body">
+											<h5 class="media-heading">John Doe</h5>
+											<p>Morbi molestie lorem quis accumsan elementum. Morbi condimentum nisl iaculis, laoreet risus sed, porta neque. Proin mi leo, dapibus at ligula a, aliquam consectetur metus.</p>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="panel-footer">
+							<form>
+								<div class="input-group">
+									<input type="text" class="form-control bg-silver" placeholder="Ingresar el mensaje" />
+									<span class="input-group-btn">
+										<button class="btn btn-primary" type="button"><i class="fa fa-pencil"></i></button>
+									</span>
+								</div>
+							</form>
+                                                </div>
+					</div>
+				</div>
+					    
+                </div>
+            <!-- end row -->
+        <!-- end row -->
+    </div>
+<!-- end #content -->
