843c181804f3d0227eab8174212e2d966c20f041
Carlos Gallardo <Carlos Gallardo@SNAP-UIO-CGALLARDO>
2015-06-09 15:10:50 -0500

    cambio en dashboard admin

--- protected/controllers/DashboardController.php
+++ protected/controllers/DashboardController.php
@@ -9,10 +9,6 @@ class DashboardController extends Controller {
         $datosTotalTramites=$model->getTotalTramite();
         $datosRankingTramites=$model->getRankingTramite();
         $this->layout = 'main-admin';
-        //envio los datos al index en tramite institucion
-        //$this->render('dashboard_admin',  compact("datos"));
-        //$datosRankingTramites = 20;
-        // $datosTotalTramites = 5;
         $this->render('dashboard_admin', compact('datosTotalTramites','datosRankingTramites'));
     }
 
