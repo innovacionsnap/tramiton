49bebf9d3ade319e63d8a383ada0a158da99ae08
fernando.ortega <fernando.ortega@PC-W7-API-2-217.snap.gob>
2015-07-30 10:59:54 -0500

    Total de trámites en Dashboard

--- themes/tramiton/views/datosTramite/index.php
+++ themes/tramiton/views/datosTramite/index.php
@@ -1,7 +1,8 @@
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/assets/plugins/DataTables/css/data-table.css" rel="stylesheet" />
 <div id="content" class="content">
     <!-- begin breadcrumb -->
     <ol class="breadcrumb pull-right">
-        <li><a href="<?php echo Yii::app()->getBaseUrl();?>/dashboard/index">Home</a></li>
+        <li><a href="<?php echo Yii::app()->getBaseUrl(); ?>/dashboard/index">Home</a></li>
         <li class="active">Total Trámites</li>
     </ol>
     <!-- end breadcrumb -->
@@ -14,56 +15,71 @@
 
     <!-- begin row -->
     <div class="row">
-        <div class="panel panel-inverse">
-            <div class="panel-heading">
-                Total de Trámites
-            </div>
-            <div class="panel-body">
-                <div id="data-table_wrapper" class="dataTables_wrapper no-footer">
-                    <table id="data-table" class="table table-striped table-bordered nowrap dataTable no-footer dtr-inline" width="100%" role="grid" aria-describedby="data-table_info" style="width: 100%;">
-                        <thead>
-                            <tr role="row">
-                                <th class="sorting_asc" tabindex="0" aria-controls="data-table" rowspan="1" colspan="1" style="width: 148px;" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">No.</th>
-                                <th class="sorting" tabindex="0" aria-controls="data-table" rowspan="1" colspan="1" style="width: 215px;" aria-label="Fecha de Registro: activate to sort column ascending">Fecha de Registro</th>
-                                <th class="sorting" tabindex="0" aria-controls="data-table" rowspan="1" colspan="1" style="width: 196px;" aria-label="Trámite: activate to sort column ascending">Trámite</th>
-                                <th class="sorting" tabindex="0" aria-controls="data-table" rowspan="1" colspan="1" style="width: 124px;" aria-label="Entidad: activate to sort column ascending">Entidad</th>
-                                <th class="sorting" tabindex="0" aria-controls="data-table" rowspan="1" colspan="1" style="width: 88px;" aria-label="Sector: activate to sort column ascending">Sector</th>
-                                <th class="sorting" tabindex="0" aria-controls="data-table" rowspan="1" colspan="1" style="width: 88px;" aria-label="Usuario: activate to sort column ascending">Usuario</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $cont= 1;
-                            foreach ($tramites as $tramite):
-                                ?>
-                                <tr class="gradeA odd" role="row">
-                                    <td class="sorting_1"><?php echo $cont; ?></td>
-                                    <td class="sorting_1"><?php echo $tramite['datt_fecharegistro'] ?></td>
-                                    <td class="sorting_1"><?php echo $tramite['datt_otronombretramite'] ?></td>
-                                    <td class="sorting_1"><?php echo $tramite['trai_id'] ?></td>
-                                    <td class="sorting_1"><?php echo $tramite['trai_id'] ?></td>
-                                    <td class="sorting_1"><?php //echo $tramite->usu['usu_nombreusuario'] ?></td>
+        <!-- begin col-12 -->
+        <div class="col-md-12">
+            <!-- begin panel -->
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <div class="panel-heading-btn">
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+                    </div>
+                    <h4 class="panel-title">Trámites</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table id="data-table" class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>N°</th>
+                                    <th>Fecha de Registro</th>
+                                    <th>Trámite</th>
+                                    <th>Entidad</th>
+                                    <th>Sector</th>
+                                    <th>Usuario</th>
                                 </tr>
-
+                            </thead>
+                            <tbody>
                                 <?php
-                                $cont++;
-                            endforeach;
-                            ?>
+                                $cont = 1;
+                                foreach ($tramites as $tramite):
+                                    ?>
+                                    <tr class="odd gradeA">
+                                        <td class="sorting_1"><?php echo $cont; ?></td>
+                                        <td class="sorting_1"><?php echo $tramite['datt_fecharegistro'] ?></td>
+                                        <td class="sorting_1"><?php echo $tramite['datt_otronombretramite'] ?></td>
+                                        <td class="sorting_1"><?php echo $tramite['trai_id'] ?></td>
+                                        <td class="sorting_1"><?php echo $tramite['trai_id'] ?></td>
+                                        <td class="sorting_1"><?php echo $tramite->usu['usu_nombreusuario'] ?></td>
+                                    </tr>
 
-                            <!--<tr class="gradeA odd" role="row">
-                                <td class="sorting_1">Gecko</td>
-                                <td>Firefox 1.0</td>
-                                <td>Win 98+ / OSX.2+</td>
-                                <td>1.7</td>
-                                <td>A</td>
-                            </tr>-->
+                                    <?php
+                                    $cont++;
+                                endforeach;
+                                ?>
 
-                        </tbody>
-                    </table>
-
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
+            <!-- end panel -->
         </div>
+        <!-- end col-12 -->
     </div>
 </div>
+<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . 'assets/plugins/DataTables/js/jquery.dataTables.js', CClientScript::POS_END); ?>	
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . 'assets/js/table-manage-default.demo.min.js', CClientScript::POS_END); ?>	
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . 'assets/js/apps.min.js', CClientScript::POS_END); ?>	
 
+
+<!-- ================== END PAGE LEVEL JS prueba ================== -->
+<script>
+    $(document).ready(function () {
+        App.init();
+        TableManageDefault.init();
+    });
+</script>
