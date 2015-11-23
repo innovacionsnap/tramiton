471cda764934996f04ed78affa16a13f562cb324
Daniel Tapia <danieltapiamoreno@gmail.com>
2015-11-23 09:55:42 -0500

    solución responsive

--- themes/tramiton/views/solucion/solucion_logueado.php
+++ themes/tramiton/views/solucion/solucion_logueado.php
@@ -1,24 +1,31 @@
 <style>
+    body{
+        padding-top: 0px;
+    }
+    .row{
+        padding: 0px;
+    }
+    #content{
+        padding-top: 0px !important;
+    }
     .btn-comentario{
-        margin-top: 30px;
-        margin-left: -10px;
+        /*margin-top: 30px;*/
+        /*margin-left: -10px;*/
     }
     .tarea{
         resize: none;
         border-radius: 8px;
     }
-    #header{
-        display:none;
-    }
     #contenido{
         padding-top: 0px;
-
+        padding-bottom: 0px;
     }
-    #footer{
-        display: none;
+    #footer, .pace, #header{
+        display:none;
     }
-    .pace{
-        display: none;
+    .page-header{
+        margin-top: 10px;
+        margin-bottom: 0px;
     }
 </style>
 <div id="contenido" class="content" style="height: auto">
@@ -28,26 +35,80 @@
 
     <!-- end page-header -->
 
+    <div class="row" style="padding-top:10px; padding-bottom:20px;">
+        <div class="col-xs-9 col-sm-8 col-sm-offset-1">
+            <span class="userimage"><img src="<?php echo (Yii::app()->theme->baseUrl . '/assets/img/users/' . $imagen_usuario); ?>" width="34px" height="34px"></span>
+            <span><?php echo $usuario_solucion; ?></span>
+            <?php
+            if ($tramite[0]['tra_nombre'] == 'Otro...') {
+                $nom_tramite = $experiencia['datt_otronombretramite'];
+            } else {
+                $nom_tramite = $tramite[0]['tra_nombre'];
+            }
+            ?>
+        </div>
+            
+        <div class="col-xs-3 col-sm-2">
+            <span class="pull-right text-muted" id="vista"><?php echo $vistas; ?></span>
+        </div>
+    </div>
 
     <div class="row">
-        <div class="col-md-8 col-md-offset-2" style="background-color: #fff; height: auto; border-radius: 8px; padding: 20px">
-            <div>
-                <span class="userimage"><img src="<?php echo (Yii::app()->theme->baseUrl . '/assets/img/users/' . $imagen_usuario); ?>" width="34px" height="34px"></span>
-                <span class="pull-right text-muted" id="vista"><?php echo $vistas; ?></span>
-                <span><?php echo $usuario_solucion; ?></span>
-                <?php
-                if ($tramite[0]['tra_nombre'] == 'Otro...') {
-                    $nom_tramite = $experiencia['datt_otronombretramite'];
-                } else {
-                    $nom_tramite = $tramite[0]['tra_nombre'];
-                }
-                ?>
-                <span><h5>Trámite:</h5><p><?php echo $nom_tramite; ?></p></span>
-                <span><h5>Experiencia:</h5><p><?php echo $experiencia['datt_experiencia'] ?></p></span>
-                <!--<span><h4><?php //echo $solucion["sol_titulo"]      ?></h4></span>-->
-                <span><h5>Solución Propuesta:</h5><p><?php echo $solucion['sol_descripcion']; ?></p></span>
+        <div class="col-sm-2 col-sm-offset-1">
+            <h5>Trámite:</h5>
+        </div>
+        <div class="col-sm-9">
+            <span><p><?php echo $nom_tramite; ?></p></span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2 col-sm-offset-1">
+            <h5>Experiencia:</h5>
+        </div>
+
+        <div class="col-sm-9">
+            <span><p><?php echo $experiencia['datt_experiencia'] ?></p></span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2 col-sm-offset-1">
+            <h5>Solución Propuesta:</h5>
+        </div>
+        <div class="col-sm-9">
+            <span><p><?php echo $solucion['sol_descripcion']; ?></p></span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div id="megusta-interno" class="col-sm-4 col-sm-offset-1">
+            <?php
+            $verifica_likes = DashboardController::validalike($solucion['sol_id']);
+            if ($verifica_likes == true) {
+                $texto = "Me gusta";
+            } else {
+                $texto = "Ya no me gusta";
+            }
+            ?>
+            <span><?php
+                echo CHtml::AjaxLink(
+                        "<span id='texto-interno'>" . $texto . "</span>", array('dashboard/procesamegusta?sol=' . $solucion['sol_id']), array('update' => '#contalike-interno', 'complete' => "actualizarTexto('#texto-interno')"), array('id' => 'ajaxlink-interno', 'class' => 'ajaxlink-interno'));
+                ?></span>
+            <span id="contalike-interno" style="color:#005580; padding-left: 20px;"><?php echo DashboardController::getLike($solucion['sol_id']);
+                ?></span><i class="fa fa-thumbs-o-up fa-fw"></i>
+        </div>
+        <div class="col-sm-5">
+            <div style="float:right;">
+                <a href="http://www.facebook.com/sharer.php?u=<?php echo urlencode('http://172.16.42.217/tramiton/solucion/index?sol=' . $solucion['sol_id']); ?>" target="_blank" style="color: inherit;"><i class="fa fa-adjust fa-facebook"></i></a>
+                <a href="http://twitter.com/share?url=<?php echo urlencode('http://172.16.42.217/tramiton/solucion/index?sol=' . $solucion['sol_id']); ?>" target="_blank" style="color: inherit;"><i class="fa fa-adjust fa-twitter"></i></a>
+                <a href="https://plus.google.com/share?url=<?php echo urlencode('http://172.16.42.217/tramiton/solucion/index?sol=' . $solucion['sol_id']); ?>" target="_blank" style="color: inherit;"><i class="fa fa-adjust fa-google-plus"></i></a>
             </div>
-            <div id ="megusta-interno" style="padding: 10px 0px;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+            <!-- <div id ="megusta-interno" style="padding: 10px 0px;">
                 <?php
                 $verifica_likes = DashboardController::validalike($solucion['sol_id']);
                 if ($verifica_likes == true) {
@@ -67,7 +128,7 @@
                     <a href="http://twitter.com/share?url=<?php echo urlencode('http://172.16.42.217/tramiton/solucion/index?sol=' . $solucion['sol_id']); ?>" target="_blank" style="color: inherit;"><i class="fa fa-adjust fa-twitter"></i></a>
                     <a href="https://plus.google.com/share?url=<?php echo urlencode('http://172.16.42.217/tramiton/solucion/index?sol=' . $solucion['sol_id']); ?>" target="_blank" style="color: inherit;"><i class="fa fa-adjust fa-google-plus"></i></a>
                 </div>
-            </div>
+            </div> -->
             <div class="panel-body p-t-0" style="margin-top:5px; border-radius: 8px">
                 <h4>Comentarios</h4>
                 <div id="comentario-interno" data-scrollbar="true" style="max-height: 300px; padding-left: 20px; padding-right: 20px; background-color: #fff; border-radius:8px;overflow: auto">
@@ -75,7 +136,7 @@
                     foreach ($comentario as $dato):
                         $usuario_comentario = SolucionController::getUsuario($dato['usu_id']);
                         ?>
-                        <div class="row" style="border-bottom: 1px solid #f1f1f1; text-align: left; padding-top: 10px;"><p><font style="color:#348fe2;"><?php echo $usuario_comentario; ?></font><font><?php echo " " . $dato['com_descripcion']; ?></font></p></div>
+                        <div class="row" style="border-bottom: 1px solid #f1f1f1; text-align: left; padding-top: 10px;"><p><font style="color:#348fe2; word-wrap: break-word;"><?php echo $usuario_comentario; ?></font><font><?php echo " " . $dato['com_descripcion']; ?></font></p></div>
                     <?php endforeach; ?>
                 </div>
                 <!--<div id="comenta-interno" data-scrollbar="true" style="font-size: 12px;max-height: 100px;margin-bottom: 0px;padding-left: 16px;padding-right: 16px; overflow: auto"><?php //DashboardController::getComentario($datoSolucion['sol_id'])      ?></div>-->
@@ -94,6 +155,7 @@
                 </div>
             </div>
         </div>
+        <!-- end col -->
     </div>
 </div>
 
