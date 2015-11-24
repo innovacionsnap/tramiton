37cd285c8d8e7bb9d47aab978c52e3a7db230102
csgallardof <csgallardof@gmail.com>
2015-11-24 15:46:47 -0500

    cambios de ciudadano usuario 24112015 15:46

--- themes/tramiton/views/ciudadano/viewTramite_Usuario2.php
+++ themes/tramiton/views/ciudadano/viewTramite_Usuario2.php
@@ -20,100 +20,95 @@
 
 <!-- begin #content -->
         <div id="content" class="content">
+            <!-- begin breadcrumb -->
+            <ol class="breadcrumb pull-right">
+                <li><a href="javascript:;">Home</a></li>
+                <li><a href="javascript:;">Extra</a></li>
+                <li class="active">Profile Page</li>
+            </ol>
             <!-- end breadcrumb -->
-            <h1 class="page-header">Detalle trámite</h1>
-
-            <div class="row">
-            <!-- <div class="table-responsive"> -->
-                <table class="table table-profile" style="margin:auto;">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>
-                                <?php foreach ($datosTramite_Usuario as $dato_tramite_detalle) ?>
-                                <h4><?php echo $dato_tramite_detalle["ins_nombre"] ?><small><strong>Registro:</strong> <?php echo $dato_tramite_detalle["datt_fecharegistro"] ?></small></h4>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="highlight">
-                            <td class="field">Tramite</td>
-                            <td><?php echo $dato_tramite_detalle["tra_nombre"] ?></td>
-                        </tr>
-                        <tr class="divider">
-                            <td colspan="2"></td>
-                        </tr>
-                        <tr>
-                            <td class="field">Provincia</td>
-                            <td><?php echo $dato_tramite_detalle["pro_nombre"] ?></td>
-                        </tr>
-                        <tr>
-                            <td class="field">Canton</td>
-                            <td><?php echo $dato_tramite_detalle["can_nombre"] ?></td>
-                        </tr>
-                        <tr>
-                            <td class="field">Fecha </td>
-                            <td><?php echo $dato_tramite_detalle["datt_fecharegistro"] ?></td>
-                        </tr>
-                        <tr>
-                            <td class="field">U. Prestado </td>
-                            <td><?php echo $dato_tramite_detalle["datt_unidadprestadora"] ?></td>
-                        </tr>
-                        <tr class="divider">
-                            <td colspan="2"></td>
-                        </tr>
-                        <tr class="highlight">
-                            <td class="field">Experiencia</td>
-                            <td><?php echo $dato_tramite_detalle["datt_experiencia"] ?></td>
-                        </tr>
-                        <tr class="divider">
-                            <td colspan="2"></td>
-                        </tr>
-                        
-                        
-                    </tbody>
-                </table>
-            <!-- </div> -->
-            </div>
+            <!-- begin page-header -->
+            <h1 class="page-header">Detalle tramite <small></small></h1>
+            <!-- end page-header -->
+            <!-- begin profile-container -->
+            <div class="profile-container">
                 <!-- begin profile-section -->
                 <div class="profile-section">
-                    <!-- begin row -->
-                    <div class="row">
-                        <!-- begin col-4 -->
-                        <div class="col-md-12">
-                            <h4 class="title">Soluciones</small></h4>
-                            <!-- begin scrollbar -->
-                            <div data-scrollbar="true" data-height="280px" class="bg-silver" style="border-radius:10px;">
-                                <!-- begin chats -->
-                                <ul class="chats">
-                                    
-                                     
-                                    <?php foreach ($datosTramite_Solucion as $dato_tramite_solucion){ ?>
-                                   
-                                    <li class="left">
-                                        <span class="date-time"><?php echo $dato_tramite_solucion["sol_fecha"] ?></span>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="../solucion/index?sol=<?php echo  $dato_tramite_solucion['sol_id'] ?>" class="solucion"><?php echo $dato_tramite_solucion["sol_titulo"] ?></a>
-                                        <a href="javascript:;" class="image"><img src="<?php echo URL_IMG . $this->_datosUser->usu_imagen; ?>" alt="" /></a>
-                                        <div class="message">
-                                           <?php echo $dato_tramite_solucion["sol_descripcion"] ?>
-                                        </div>
-                                    </li>
-                                   <?php } ?>
-                                   <?php //$usuario = Usuario::model()->findByPk(id);
-                                   //echo $usuario;
-                                    ?>
-                                    
-                                </ul>
-                                <!-- end chats -->
+                   
+                    <!-- begin profile-right -->
+                    <div class="profile">
+                        <!-- begin profile-info -->
+                        <div class="profile-info">
+                            <!-- begin table -->
+                            <div class="table-responsive">
+                                <table class="table table-profile">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>
+                                                <?php foreach ($datosTramite_Usuario as $dato_tramite_detalle) ?>
+                                                <h4><?php echo $dato_tramite_detalle["ins_nombre"] ?><small><strong>Registro:</strong> <?php echo $dato_tramite_detalle["datt_fecharegistro"] ?></small></h4>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="highlight">
+                                            <td class="field">Tramite</td>
+                                            <td><?php echo $dato_tramite_detalle["tra_nombre"] ?></td>
+                                        </tr>
+                                        <tr class="divider">
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="field">Provincia</td>
+                                            <td><?php echo $dato_tramite_detalle["pro_nombre"] ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="field">Canton</td>
+                                            <td><?php echo $dato_tramite_detalle["can_nombre"] ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="field">Fecha </td>
+                                            <td><?php echo $dato_tramite_detalle["datt_fecharegistro"] ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="field">U. Prestado </td>
+                                            <td><?php echo $dato_tramite_detalle["datt_unidadprestadora"] ?></td>
+                                        </tr>
+                                      
+                                        <tr>
+                                            <td class="field">Experiencia</td>
+                                            <td><?php echo $dato_tramite_detalle["datt_experiencia"] ?></td>
+                                        </tr>
+                                        <tr class="divider">
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr class="highlight">
+                                            <td class="field">Soluciones</td>
+                                            <td>
+                                                <?php foreach ($datosTramite_Solucion as $dato_tramite_solucion){ ?>
+                                                    <a href="../solucion/index?sol=<?php echo  $dato_tramite_solucion['sol_id'] ?>" class="solucion"><?php echo $dato_tramite_solucion["sol_titulo"] ?></a>
+                                                    <div class="message">
+                                                        <?php echo $dato_tramite_solucion["sol_descripcion"] ?>
+                                                    </div>
+                                                    <br>
+
+                                                     <a href="../solucion/index?sol=<?php echo  $dato_tramite_solucion['sol_id'] ?>" class="btn btn-info solucion" role="button">Responder</a>
+                                                <?php } ?>
+
+                                            </td>
+                                        </tr> 
+                                    </tbody>
+                                </table>
                             </div>
-                            <!-- end scrollbar -->
+                            <!-- end table -->
                         </div>
-                        <!-- end col-4 -->
-                       
+                        <!-- end profile-info -->
                     </div>
-                    <!-- end row -->
+                    <!-- end profile-right -->
                 </div>
                 <!-- end profile-section -->
+                
             </div>
             <!-- end profile-container -->
         </div>
