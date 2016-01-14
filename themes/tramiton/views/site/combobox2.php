c1ba22973fb7731ba599f75b68cbd6a21861c642
wilsonaizaga <wil5858@hotmail.com>
2016-01-14 11:37:15 -0500

    arreglo site  tabla temporal

--- themes/tramiton/views/site/combobox2.php
+++ themes/tramiton/views/site/combobox2.php
@@ -1,5 +1,5 @@
 <?php
-include '../../../../protected/extensions/validacion/config.inc.php';    
+include '../../../../protected/extensions/validacion/config.inc.php';
 
 
 
@@ -16,14 +16,14 @@ if ($_GET['buscar_institucion']=="institucion")
 	$var_institucion = $_GET['id_institucion'];
 	//echo "Id: institucion".$var_institucion."<br>";
 	
-	if ($var_institucion==''){
+        if ($var_institucion==''){
 
 		//echo "Seleccionar una institución";
 		
 	}else {
-			
-		$consulta_tramite = "select trai.trai_id,ins.ins_id,ins.ins_nombre,tra.tra_nombre from tramite tra, tramite_institucion trai, institucion ins where tra.tra_id = trai.tra_id and trai.ins_id = ins.ins_id and ins.ins_id='".$var_institucion."' order by trai.tra_id,tra.tra_nombre";	
 		
+            $consulta_tramite = "select trai.trai_id,ins.ins_id,ins.ins_nombre,tra.tra_nombre from tramite tra, tramite_institucion trai, institucion ins where tra.tra_id = trai.tra_id and trai.ins_id = ins.ins_id and ins.ins_id='".$var_institucion."' order by trai.tra_id,tra.tra_nombre";	
+	
 		//echo "<br>".$consulta_tramite."<br>";
 		$resultado_tramite = pg_query($con, $consulta_tramite) or die("Error en la Consulta SQL");
 	    $numReg = pg_num_rows($resultado_tramite);
@@ -36,19 +36,19 @@ if ($_GET['buscar_institucion']=="institucion")
 			echo "<div class='col-md-12'> <div class='form-group block1'>";
 			echo "<label>Trámite <i class='fa fa-question-circle ayuda' data-toggle='tooltip' data-placement='right' title='Seleccione el trámite realizado'></i> </label>";
 		
-		
-			if($numReg == 1 and $fila['tra_nombre'] =='Otro...'){
-				 echo "<input type='text' name='otro_tramite' onkeyup = 'Validate(this)' placeholder='Escriba otro tramite' class='campo-panel2 form-registro' data-parsley-group='wizard-step-1' required />";
-		    }else{
-		    	echo "<select class='campo-panel2 form-registro' data-parsley-group='wizard-step-1' name='id_tramite' id='id_tramite' required>";
-				
-				while ($fila2=pg_fetch_array($resultado_tramite2)){
-					echo "<option value=".$fila2['ins_id'].">".$fila2['tra_nombre']."</option>";
-				}
+	                echo "<select class='form-control' data-parsley-group='wizard-step-1' name='id_tramite2' id='id_tramite2'  required>";
+	                while ($fila2=pg_fetch_array($resultado_tramite2)){
+					echo "<option value=".$fila2['trai_id'].">".$fila2['tra_nombre']." </option>";
+	                }
 				echo "</select>";
-			}
-			echo "</div>";		
-	}
+	                        if($numReg == 1) {//and $fila['tra_nombre'] ==='Otro...'){
+				echo "<br>";
+	                         echo "<input type='text' name='otro_tramite' onkeyup = 'Validate(this)' placeholder='Escriba otro tramite' class='form-control' data-parsley-group='wizard-step-1' required />";
+				 
+	                        }     
+                                echo $fila['tra_nombre'];
+                        echo "</div>";                       
+                                         }
 	
 	
 }
