5a21b8ce1955771da0b78c9bda17b2517c4bce86
Daniel Tapia <danieltapiamoreno@gmail.com>
2016-03-15 09:48:05 -0500

    correciones que es tramiton (firefox)

--- themes/tramiton/views/site/que_es_tramiton.php
+++ themes/tramiton/views/site/que_es_tramiton.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link rel="stylesheet" href="../../assets/css/bigvideo.css">
-    <script src="../../assets/js/bigvideo/modernizr-2.5.3.min.js"></script>
+    <!-- <script src="../../assets/js/bigvideo/modernizr-2.5.3.min.js"></script> -->
 
 </head>
 <body>
@@ -18,25 +18,25 @@
          simplificación de los trámites para impulsar la calidad, eficiencia, eficacia y calidez en la gestión pública.</p>
     </header>
 
-    <div class="wrapper">
+    <!-- <div class="wrapper"> -->
         <!-- <div style="-webkit-filter:sepia(100%);" class="screen" id="screen-1" data-video="../../assets/mp4/tramiton.mp4"> -->
-        <div class="screen" id="screen-1">
-            <!-- <img src="../../assets/img/que_es_tramiton_1.png" class="big-image" /> -->
+        <!-- <div class="screen" id="screen-1"> -->
+            <!-- <img id="img-tramiton" src="../../assets/img/que_es_tramiton_1.png" class="big-image" /> -->
             <!-- <h1 class="video-title">#1 Bird</h1> -->
-        </div>
-    </div>
+        <!-- </div> -->
+    <!-- </div> -->
 
     <!-- BigVideo Dependencies -->
 
-    <script src="../../assets/js/bigvideo/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery-1.8.1.min.js"><\/script>')</script>
-    <script src="../../assets/js/bigvideo/jquery-ui-1.8.22.custom.min.js"></script>
-    <script src="../../assets/js/bigvideo/jquery.imagesloaded.min.js"></script>
-    <script src="../../assets/js/bigvideo/video.js"></script>
+    <!-- <script src="../../assets/js/bigvideo/jquery.min.js"></script> -->
+    <!-- <script>window.jQuery || document.write('<script src="js/jquery-1.8.1.min.js"><\/script>')</script> -->
+    <!-- <script src="../../assets/js/bigvideo/jquery-ui-1.8.22.custom.min.js"></script> -->
+    <!-- <script src="../../assets/js/bigvideo/jquery.imagesloaded.min.js"></script> -->
+    <!-- <script src="../../assets/js/bigvideo/video.js"></script> -->
 
     <!-- BigVideo -->
-    <script src="../../assets/js/bigvideo/bigvideo.js"></script>
-    <script src="../../assets/js/bigvideo/jquery.transit.min.js"></script>
-    <script src="../../assets/js/bigvideo/jsBigVideo.js"></script>
+    <!-- <script src="../../assets/js/bigvideo/bigvideo.js"></script> -->
+    <!-- <script src="../../assets/js/bigvideo/jquery.transit.min.js"></script> -->
+    <!-- <script src="../../assets/js/bigvideo/jsBigVideo.js"></script> -->
 </body>
 </html>
