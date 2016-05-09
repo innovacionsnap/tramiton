94ab4638f6e5eaf27019102896188fd050821485
Daniel Tapia <danieltapiamoreno@gmail.com>
2016-05-09 12:34:03 -0500

    correcion login

---
+++ themes/tramiton/views/site/form_login.php
@@ -0,0 +1,18 @@
+<form id="form-login" role="form">
+  <div class="form-group">
+    <label for="email">Usuario:</label>
+    <input type="text" class="form-control" id="">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Contraseña:</label>
+    <input type="password" class="form-control" id="">
+  </div>
+
+  <div class="checkbox">
+    <label><input type="checkbox"> Recordarme</label>
+
+  </div>
+  <button type="button" class="btn btn-info">Ingresar</button>
+
+  ¿No dispones de una cuenta en Tramitón? <a href="site/registro">Regístrate aquí.</a>
+</form>
--- themes/tramiton/views/site/index.php
+++ themes/tramiton/views/site/index.php
@@ -2,6 +2,7 @@
 $baseUrl = Yii::app()->theme->baseUrl;
 ?>
 
+
 <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
   <div class="modal-dialog modal-dialog-center">
     <div class="modal-content">
@@ -31,14 +32,20 @@ $baseUrl = Yii::app()->theme->baseUrl;
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
+<!-- $bandera -->
+
 <div id="home" class="container-fluid p-t-10 p-l-15 p-r-15 p-b-30">
     <div class="row">
-      <!--<h4 class="text-left p-l-15" style="font-weight: bold;">Registra tu caso</h4>-->
-      <h4 class="text-left p-l-15">.</h4>
+      <!-- <h4 class="text-left p-l-15" style="font-weight: bold;">Registra tu caso</h4> -->
         <div id="col-formulario" class="col-sm-7">
-            <?php
-            $this->renderPartial('form_caso', true, false);
-            ?>
+          <?php
+            if(true){
+              $this->renderPartial('form_login', true, false);
+            }
+            else{
+              $this->renderPartial('form_caso', true, false);
+            }
+          ?>
         </div>
 
         <div id="noticias-2" class="col-sm-5 text-center">
@@ -81,199 +88,199 @@ $baseUrl = Yii::app()->theme->baseUrl;
         </div>
     </div>
 
-    <div class="row">
+    <div class="col-md-6 m-t-30">
+      <h2 style="color:#325972;" class="text-left titulo-estadistica m-t-0 m-l-0">Instituciones con propuestas de solución</h2>
+
       <?php
-      $nombreac="serie1";
-      $nombreac2="serie2";
-      $nombreac3="serie3";
-      $nombreac4="serie4";
-      $nombreac5="serie5";
-      $nombreac6="serie6";
-      $nombreac7="serie7";
-      $nombreac8="serie8";
-      $nombreac9="serie9";
-      $nombreac10="serie10";
-
-      $nacc=1;
-      $i=1;
-
-      foreach ($totalAccionesnom as $nacc) {
-
-           if($i==1)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac=array('name' => $name, 'y' => 59.33);
-
-           }
-
-           if($i==2)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac2=array('name' => $name, 'data' => array($p5));
-
-           }
-           //$i++;
-
-           if($i==3)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac3=array('name' => $name, 'data' => array($p5));
-
-           }
-           if($i==4)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac4=array('name' => $name, 'data' => array($p5));
-
-           }
-           if($i==5)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac5=array('name' => $name, 'data' => array($p5));
-
-           }
-           if($i==6)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac6=array('name' => $name, 'data' => array($p5));
-
-           }
-
-           if($i==7)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac7=array('name' => $name, 'data' => array($p5));
-
-           }
-
-           if($i==8)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac8=array('name' => $name, 'data' => array($p5));
-
-           }
-
-           if($i==9)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac9=array('name' => $name, 'data' => array($p5));
-
-           }
-
-           if($i==10)
-           {
-           $p5 = $nacc['total']+0;
-           $name= $nacc['nombre'];
-           $nombreac10=array('name' => $name, 'data' => array($p5));
-
-           }
-           $i++;
-
-       }
-
-
       $this->Widget('ext.highcharts.HighchartsWidget', array(
-   'options' => "{
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Browser market shares January, 2015 to May, 2015'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
+       'options' => "{
+        chart: {
+            type: 'pyramid',
+            marginRight: 100
+        },
+        title: {
+            text: 'Sales pyramid',
+            x: -50
+        },
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b> ({point.y:,.0f})',
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                    softConnector: true
                 }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: [{
-                    name: '" . $nombreac['name'] . "',
-                    y: " . $nombreac['y'] . "
-                }, {
-                    name: 'Chrome',
-                    y: 24.03,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Firefox',
-                    y: 10.38
-                }, {
-                    name: 'Safari',
-                    y: 3.77
-                }, {
-                    name: 'Opera',
-                    y: 0.91
-                }, {
-                    name: 'Proprietary or Undetectable',
-                    y: 0.2
-                }]
-            }]
-        }"
-));
-       ?>
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        series: [{
+            name: 'Unique users',
+            data: [
+                ['Website visits',      15654],
+                ['Downloads',            4064],
+                ['Requested price list', 1987],
+                ['Invoice sent',          976],
+                ['Finalized',             846]
+            ]
+        }]
+    }"
+      ));
+    ?>
     </div>
 
-    <!-- <div class="row"> -->
-      <?php
-
-
-      // $this->Widget('ext.highcharts.HighchartsWidget', array(
-      //   'options' => array(
-      //     'chart' => array('plotBackgroundColor' => null, 'plotBorderWidth' => null, 'plotShadow' => false, 'type' => 'pie'),
-      //     'title' => array('text' => 'Fruit Consumption'),
-      //     'tooltip' => array('pointFormat' => '{series.name}: <b>{point.percentage:.1f}%</b>'),
-      //     'plotOptions' => array('pie' => array(
-      //       'allowPointSelect' => true,
-      //       'cursor' => 'pointer',
-      //       'dataLabels' => array('enabled' => false),
-      //       'showInLegend' => true
-      //     )),
-      //     'series' => array(
-      //       'name' => 'Instituciones',
-      //       'colorByPoint' => true,
-      //       'data' => array(
-      //         array('name' => 'qwer', 'y' => 56.24, 'sliced' => true, 'selected' => true),
-      //         array('name' => 'asfd', 'y' => 5.24),
-      //         array('name' => 'zxcv', 'y' => 10.24)
-      //       )
-      //     )
-      //
-      //
-      //   )
-      // ));
-
-
-       ?>
-    <!-- </div> -->
-
-
     <!-- begin row -->
-    <div class="row m-t-30">
+    <div class="col-md-6 m-t-30">
         <div class="col-xs-12 p-0">
           <h2 style="color:#325972;" class="text-left titulo-estadistica m-t-0 m-l-0">Instituciones con más acciones correctivas</h2>
+          <?php
+         $nombreac="serie1";
+         $nombreac2="serie2";
+         $nombreac3="serie3";
+         $nombreac4="serie4";
+         $nombreac5="serie5";
+         $nombreac6="serie6";
+         $nombreac7="serie7";
+         $nombreac8="serie8";
+         $nombreac9="serie9";
+         $nombreac10="serie10";
 
+         $nacc=1;
+         $i=1;
+
+         foreach ($totalAccionesnom as $nacc) {
+
+              if($i==1)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac=array('name' => $name, 'data' => array($p5));
+
+              }
+
+              if($i==2)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac2=array('name' => $name, 'data' => array($p5));
+
+              }
+              //$i++;
+
+              if($i==3)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac3=array('name' => $name, 'data' => array($p5));
+
+              }
+              if($i==4)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac4=array('name' => $name, 'data' => array($p5));
+
+              }
+              if($i==5)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac5=array('name' => $name, 'data' => array($p5));
+
+              }
+              if($i==6)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac6=array('name' => $name, 'data' => array($p5));
+
+              }
+
+              if($i==7)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac7=array('name' => $name, 'data' => array($p5));
+
+              }
+
+              if($i==8)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac8=array('name' => $name, 'data' => array($p5));
+
+              }
+
+              if($i==9)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac9=array('name' => $name, 'data' => array($p5));
+
+              }
+
+              if($i==10)
+              {
+              $p5 = $nacc['total']+0;
+              $name= $nacc['nombre'];
+              $nombreac10=array('name' => $name, 'data' => array($p5));
+
+              }
+              $i++;
+
+          }
+
+          $this->Widget('ext.highcharts.HighchartsWidget', array(
+              'options' => array(
+                  'chart' => array(
+                          'plotBackgroundColor' => '#ffffff',
+                          'plotBorderWidth' => null,
+                          'plotShadow' => false,
+                          'height' => 400,
+                          'type'=>'column'
+                        ),
+
+
+
+
+                  'credits' => array('enabled' => false),
+                  'title' => array('text' => ''),
+                  'xAxis' => array(
+                      'categories' => array('INSTITUCIONES')
+                  ),
+                  'yAxis' => array(
+                      'title' => array('text' => 'ACCIONES CORRECTIVAS')
+                  ),
+
+
+                //'series'  = $accion;
+
+                  'series' => array(
+
+
+                      $nombreac,$nombreac2,$nombreac3,$nombreac4,$nombreac5,
+                      $nombreac6,$nombreac7,$nombreac8,$nombreac9,$nombreac10
+
+                      //array('name' => 'Renovacion Matricula Caducidad', 'data' => array(100)),
+                      //array('name' => 'Renovacion cédula', 'data' => array(88)),
+                      //array('name' => 'Papeleta votación', 'data' => array(70)),
+                      //array('name' => 'Certificado de defunción', 'data' => array(60)),
+                      //array('name' => 'Permiso de operación vehicular', 'data' => array(55)),
+                      //array('name' => 'Préstamos Hipotecarios', 'data' => array(45)),
+                      //array('name' => 'Citas médicas', 'data' => array(35)),
+                      //array('name' => 'Certificado de antescedentes policiales', 'data' => array(20)),
+                      //array('name' => 'Permiso de salida del país', 'data' => array(10)),
+                      //array('name' => 'Préstamos Quirografarios', 'data' => array(25)),
+                      //array('name' => $name, 'data' => array($p5))
+
+                      )
+              )
+          ));
+
+
+          ?>
         </div>
 
 
@@ -305,7 +312,7 @@ $baseUrl = Yii::app()->theme->baseUrl;
     <div class = "panel panel-inverse m-b-0 no-border">
         <div class = "panel-heading">
             <h3 class = "panel-title">
-                <a data-toggle = "collapse" href = "#acordeon-preguntas" class="p-l-15 f-18">Preguntas frecuentes</a>
+                <a data-toggle = "collapse" href = "#acordeon-preguntas" class="p-l-15 f-18">Preguntas frecuentes <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></a>
             </h3>
         </div>
         <div id = "acordeon-preguntas" class = "panel-collapse collapse">
